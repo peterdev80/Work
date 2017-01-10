c01da51518b11fe00c6e441b92a97bf64dc77d8b
peter <peter.dev@live.com>
2017-01-10 21:40:54 +0300

    88

--- MnemoMalicov/MnemoFMS/ValueBulder/ElementValue.cs
+++ MnemoMalicov/MnemoFMS/ValueBulder/ElementValue.cs
@@ -1,4 +1,4 @@
-using System;
+п»їusing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ValueBulder
 {
-    //проверка гит
+    //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ
     class ElementValue
     {
         public string NameVal { get; set; }
